4b067ec5ac5b89662fac1937b50f90ba167ee794
Shirad Salaheddine <shirads@users.noreply.github.com>
2017-02-09 17:40:53 +0400

    Add files via upload

---
+++ Arduino/DHT/DHT_NodeJSClient.ino
@@ -0,0 +1,32 @@
+#include "DHT.h"
+
+#define DHTPIN A0
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned int nTimer = 6000; // milliseconds
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  delay(1000);
+}
+
+int initialTime = millis();
+
+void loop() {
+  int newTime = millis();
+  if ((newTime-initialTime) >= nTimer) {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (!isnan(t) && !isnan(h)) {
+      Serial.print(h);
+      Serial.print(',');
+      Serial.println(t);
+    }
+    initialTime = newTime;
+  }
+}
